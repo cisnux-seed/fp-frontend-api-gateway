f6adc5746f1a2924529cfa208a7bc3f7f3bbae4a
cisnux <fajrarisqulla@gmail.com>
2025-07-10 13:53:53 +0700

    fix: reset project

--- src/services/bniAPI.ts
+++
@@ -1,5 +0,0 @@
-export const hitBNIPaymentGateway = async (method: string, amount: number) => {
-  console.log(`Mengirim ke BNI: metode=${method}, jumlah=${amount}`);
-  await new Promise((res) => setTimeout(res, 1000));
-  return { status: 'success' };
-};
